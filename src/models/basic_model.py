d427eb6e754b124a2d3e68ab12d136e87b925684
Rmojarro1 <48000819+Rmojarro1@users.noreply.github.com>
2025-02-18 20:09:36 -0800

    bulk commit

--- src/models/basic_model.py
+++ src/models/basic_model.py
@@ -1,15 +1,34 @@
 from models.model import Model
 from tensorflow.keras import Sequential, layers
-from tensorflow.keras.layers.experimental.preprocessing import Rescaling
-from tensorflow.keras.optimizers import RMSprop, Adam
+from tensorflow.keras.optimizers import RMSprop
 
 class BasicModel(Model):
     def _define_model(self, input_shape, categories_count):
-        # Your code goes here
-        # you have to initialize self.model to a keras model
-        pass
-    
+        self.model = Sequential([
+            layers.Rescaling(1./255, input_shape=input_shape),
+            layers.Conv2D(16, (3, 3), activation='relu', input_shape=input_shape),
+            layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(32, (3, 3), activation='relu'),
+            layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(64, (3, 3), activation='relu'),
+            layers.MaxPooling2D((2, 2)),
+            layers.Dropout(0.5),
+            layers.Conv2D(64, (3, 3), activation='relu'),
+            layers.MaxPooling2D((2, 2)),
+            layers.Flatten(),
+
+            # Add a dropout layer to prevent overfitting (does 20% work?)
+            layers.Dropout(0.3),
+
+            layers. Dense(28, activation='relu'),
+            layers.Dense(categories_count, activation='softmax')
+        ])
+
+
     def _compile_model(self):
-        # Your code goes here
-        # you have to compile the keras model, similar to the example in the writeup
-        pass
+
+        self.model.compile(
+            optimizer=RMSprop(learning_rate=0.001),
+            loss='categorical_crossentropy',
+            metrics=['accuracy'],
+        )
